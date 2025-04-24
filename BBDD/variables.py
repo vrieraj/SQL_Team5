7ccfb5acf377580deb60056d6b8356e6f0a227ad
Víctor Riera <vrieraj@gmail.com>
2025-04-25 00:02:39 +0200

    Update

--- BBDD/variables.py
+++ BBDD/variables.py
@@ -1,4 +1,4 @@
 import sqlite3
-conexion = sqlite3.connect("./../piezas.db")
+conexion = sqlite3.connect("./piezas.db")
 cursor = conexion.cursor()
 tablas = [('Proveedores','proveedor'),('Pieza','pieza')]
